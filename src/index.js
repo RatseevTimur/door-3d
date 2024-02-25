81731a270863357445752b8855e17d2752de85a5
RatseevTimur <95998454+RatseevTimur@users.noreply.github.com>
2024-02-26 02:41:29 +0300

    door-3d

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/door">
+      <BrowserRouter basename="/door-3d">
         <App />
       </BrowserRouter>
     </Provider>
